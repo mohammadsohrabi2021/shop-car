38b565900880f85efa084fe12cd57355704db870
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-08-20 00:11:20 +0330

    create slug categorie route done

---
+++ pages/categories/[slug].js
@@ -0,0 +1,12 @@
+import React from "react";
+import CarsList from "@/component/templates/carsList/CarsList";
+
+const Categories = () => {
+  return (
+    <div>
+      <CarsList />
+    </div>
+  );
+};
+
+export default Categories;
