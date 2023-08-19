94e4009ff4e89a5438d0129473b4f365c523dc1f
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-08-20 00:11:50 +0330

    create slug categori all route done

---
+++ pages/filter/[...slug].js
@@ -0,0 +1,13 @@
+import React from "react";
+import CarsList from "@/component/templates/carsList/CarsList";
+
+const FilteredCars = () => {
+
+  return (
+    <div>
+        <CarsList/>
+    </div>
+  );
+};
+
+export default FilteredCars;
